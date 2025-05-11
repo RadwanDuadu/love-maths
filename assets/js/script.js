2f66db0aa2486c5570fb3d0c0e8204fe4a0d0529
RadwanDuadu <mradwan444@gmail.com>
2025-05-11 13:35:52 +0100

    Add favicon and tidy code

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,12 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         });
     }
+
+    document.getElementById("answer-box").addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            checkAnswer();
+        }
+    });
     // default gameType
     runGame("addition");
         
@@ -25,6 +31,10 @@ document.addEventListener("DOMContentLoaded", function() {
  * and after the user's answer has been processed
  */
 function runGame(gameType) {
+
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
+
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
 
@@ -59,9 +69,7 @@ function checkAnswer() {
         alert(`Wrong! The correct answer was ${correctAnswer[0]}`);
         incrementWrongAnswer();
     }
-    // Clear the answer box
     // and generate new question
-    document.getElementById("answer-box").value = "";
     runGame(correctAnswer[1]);
 }
 
